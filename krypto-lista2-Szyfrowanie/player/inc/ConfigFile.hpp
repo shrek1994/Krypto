d14fabc2634150aa97436859455e147449dd867f
shrek1994 <maciej.wozny41@gmail.com>
2016-11-10 17:13:21 +0100

    player mp3

---
+++ krypto-lista2-Szyfrowanie/player/inc/ConfigFile.hpp
@@ -0,0 +1,11 @@
+#ifndef CONFIGFILE_H
+#define CONFIGFILE_H
+
+
+class ConfigFile
+{
+public:
+    ConfigFile();
+};
+
+#endif // CONFIGFILE_H
---
+++ krypto-lista2-Szyfrowanie/player/src/ConfigFile.cpp
@@ -0,0 +1,6 @@
+#include "inc/ConfigFile.hpp"
+
+ConfigFile::ConfigFile()
+{
+
+}
